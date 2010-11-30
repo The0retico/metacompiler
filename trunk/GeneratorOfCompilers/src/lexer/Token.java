7054f7d99c1248205b5fbbd2e36bbe832a5f117a
gscerbak <gscerbak@451a3750-404f-e429-ab1e-3de3acf2b62b>
2010-11-30 22:27:26 +0000

    Some obvious problems like formating, depreecated APIs etc. fixed.

--- trunk/GeneratorOfCompilers/src/lexer/Token.java
+++ trunk/GeneratorOfCompilers/src/lexer/Token.java
@@ -1,17 +1,18 @@
 package lexer;
 
 public class Token {
-	public String typeOfToken;
-	public String nameOfToken;
-	public int stored;
-	
-	public Token(String type, String name){
+	private final String typeOfToken;
+	private String nameOfToken;
+	private final int stored;
+
+	public Token(final String type) {
 		typeOfToken = type;
-		nameOfToken = name;
 		stored = 0;
 	}
-	public Token(String type){
+
+	public Token(final String type, final String name) {
 		typeOfToken = type;
+		nameOfToken = name;
 		stored = 0;
 	}
 
--- trunk/GeneratorOfCompilers/test/lexer/Test2.java
+++ trunk/GeneratorOfCompilers/test/lexer/Test2.java
@@ -1,22 +1,20 @@
 package lexer;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-
-
 public class Test2 {
-	
+
 	@Test
-	public void Symbol(){
-		Lexer lexer = new EBNFLexer(" = , | ;");
+	public final void symbol() {
+		final Lexer lexer = new EBNFLexer(" = , | ;");
 		Token token;// = new Token();
 		assertTrue(lexer.hasNextToken());
-		while (lexer.hasNextToken()){
+		while (lexer.hasNextToken()) {
 			token = lexer.getNextToken();
 			System.out.println(token.nameOfToken);
-			//assertTrue(lexer.hasNextToken());
+			// assertTrue(lexer.hasNextToken());
 		}
 	}
 }
