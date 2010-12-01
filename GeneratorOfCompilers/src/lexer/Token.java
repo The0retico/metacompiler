5f6ac7d0ef43424a24daa93e6addb1966c7db756
sarvasmartin <sarvasmartin@451a3750-404f-e429-ab1e-3de3acf2b62b>
2010-12-01 12:18:37 +0000

    changed representation of type of token and add getters, setters

--- GeneratorOfCompilers/src/lexer/Token.java
+++ GeneratorOfCompilers/src/lexer/Token.java
@@ -1,19 +1,36 @@
 package lexer;
 
 public class Token {
-	private final String typeOfToken;
+	public enum typeOfToken{DEFINITION, CONCATENATION, TERMINATION, ALTERNATION, EXCEPTION,IDENTIFIER };
+	private typeOfToken mytype; 
 	private String nameOfToken;
 	private final int stored;
 
-	public Token(final String type) {
-		typeOfToken = type;
+	public Token(final typeOfToken type) {
+		mytype = type;
 		stored = 0;
 	}
 
-	public Token(final String type, final String name) {
-		typeOfToken = type;
+	public Token(final typeOfToken type, final String name) {
+		mytype = type;
 		nameOfToken = name;
 		stored = 0;
+	}
+
+	public typeOfToken getMytype() {
+		return mytype;
+	}
+
+	public void setMytype(typeOfToken mytype) {
+		this.mytype = mytype;
+	}
+
+	public String getNameOfToken() {
+		return nameOfToken;
+	}
+
+	public void setNameOfToken(String nameOfToken) {
+		this.nameOfToken = nameOfToken;
 	}
 
 }
