d3405a2229712df2095894b1de523f47bf397126
sarvasmartin <sarvasmartin@451a3750-404f-e429-ab1e-3de3acf2b62b>
2010-12-03 00:46:31 +0000

    tests for every symbol

--- GeneratorOfCompilers/test/lexer/Test2.java
+++ GeneratorOfCompilers/test/lexer/Test2.java
@@ -1,27 +1,132 @@
 package lexer;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class Test2 {
 
-	@Test
-	public final void symbol() {
-		final Lexer lexer = new EBNFLexer(" = , | ;");
-		Token token;// = new Token();
-		final Token.TokenType[] probablyInput = { Token.TokenType.DEFINITION,
-				Token.TokenType.CONCATENATION, Token.TokenType.ALTERNATION,
-				Token.TokenType.TERMINATION };
-		final Token.TokenType[] input = new Token.TokenType[4];
-		int i = 0;
+	
+	/**Test of white space symbol
+	 * 
+	 */
+	/*@Test
+	public final void wspSymbol() {
+		final Lexer lexer = new EBNFLexer(" ");
+		Token token = null;// = new Token();
 		assertTrue(lexer.hasNextToken());
-		while (lexer.hasNextToken()) {
+		try {
 			token = lexer.getNextToken();
-			input[i] = token.getType();
-			i++;
+		} catch (UndefinedSymbolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		assertArrayEquals(probablyInput, input);
+		assertEquals(Token.TokenType.DEFINITION, token.getType());
+	}*/
+	
+	/**Test of definition symbol in EBNF "="
+	 * 
+	 */
+	@Test
+	public final void defSymbol() {
+		final Lexer lexer = new EBNFLexer("=");
+		Token token = null;// = new Token();
+		assertTrue(lexer.hasNextToken());
+		try {
+			token = lexer.getNextToken();
+		} catch (UndefinedSymbolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertEquals(Token.TokenType.DEFINITION, token.getType());
 	}
+	
+	/**Test of concatenation symbol in EBNF ","
+	 * 
+	 */
+	@Test
+	public final void conSymbol() {
+		final Lexer lexer = new EBNFLexer(",");
+		Token token = null;// = new Token();
+		assertTrue(lexer.hasNextToken());
+		try {
+			token = lexer.getNextToken();
+		} catch (UndefinedSymbolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertEquals(Token.TokenType.CONCATENATION, token.getType());
+	}
+	
+	/**Test of termination symbol in EBNF ";"
+	 * 
+	 */
+	@Test
+	public final void terSymbol() {
+		final Lexer lexer = new EBNFLexer(";");
+		Token token = null;// = new Token();
+		assertTrue(lexer.hasNextToken());
+		try {
+			token = lexer.getNextToken();
+		} catch (UndefinedSymbolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertEquals(Token.TokenType.TERMINATION, token.getType());
+	}
+	
+	
+	/**Test of alternation symbol in EBNF "|"
+	 * 
+	 */
+	@Test
+	public final void altSymbol() {
+		final Lexer lexer = new EBNFLexer("|");
+		Token token = null;// = new Token();
+		assertTrue(lexer.hasNextToken());
+		try {
+			token = lexer.getNextToken();
+		} catch (UndefinedSymbolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertEquals(Token.TokenType.ALTERNATION, token.getType());
+	}
+	
+	/**Test of exception symbol in EBNF "-"
+	 * 
+	 */
+	@Test
+	public final void excSymbol() {
+		final Lexer lexer = new EBNFLexer("-");
+		Token token = null;// = new Token();
+		assertTrue(lexer.hasNextToken());
+		try {
+			token = lexer.getNextToken();
+		} catch (UndefinedSymbolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertEquals(Token.TokenType.EXCEPTION, token.getType());
+	}
+	
+	/**Test of repetition symbol in EBNF "*"
+	 * 
+	 */
+	@Test
+	public final void repSymbol() {
+		final Lexer lexer = new EBNFLexer("*");
+		Token token = null;// = new Token();
+		assertTrue(lexer.hasNextToken());
+		try {
+			token = lexer.getNextToken();
+		} catch (UndefinedSymbolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertEquals(Token.TokenType.REPETITION, token.getType());
+	}
+	
 }
