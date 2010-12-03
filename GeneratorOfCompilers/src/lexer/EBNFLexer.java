bed1eab67525d8a020645728fa5ed9cb974863ed
sarvasmartin <sarvasmartin@451a3750-404f-e429-ab1e-3de3acf2b62b>
2010-12-03 00:29:54 +0000

    switch, result

--- GeneratorOfCompilers/src/lexer/EBNFLexer.java
+++ GeneratorOfCompilers/src/lexer/EBNFLexer.java
@@ -22,29 +22,36 @@ public class EBNFLexer implements Lexer {
 	}
 
 	@Override
-	public final Token getNextToken() {
+	public final Token getNextToken() throws UndefinedSymbolException {
+		Token result = null;
 		for (int i = position; i < inputText.length(); i++) {
 			if (inputText.charAt(i) != WHITE_SPACE) {
-				if (inputText.charAt(i) == '=') {
-					position = i + 1;
-					return new Token(Token.TokenType.DEFINITION);
+				switch (inputText.charAt(i)){
+					case '=':   position = i + 1;
+								result = new Token(Token.TokenType.DEFINITION);
+								break;
+					case ',': 	position = i + 1;
+								result = new Token(Token.TokenType.CONCATENATION);
+								break;
+					case ';':   position = i + 1;
+								result = new Token(Token.TokenType.TERMINATION);
+								break;
+					case '|': 	position = i + 1;
+								result = new Token(Token.TokenType.ALTERNATION);
+								break;
+					case '-':   position = i + 1;
+								result = new Token(Token.TokenType.EXCEPTION);
+								break;
+					case '*': 	position = i + 1;
+								result = new Token(Token.TokenType.REPETITION);
+								break;
 				}
-				if (inputText.charAt(i) == ',') {
-					position = i + 1;
-					return new Token(Token.TokenType.CONCATENATION);
-				}
-				if (inputText.charAt(i) == ';') {
-					position = i + 1;
-					return new Token(Token.TokenType.TERMINATION);
-				}
-				if (inputText.charAt(i) == '|') {
-					position = i + 1;
-					return new Token(Token.TokenType.ALTERNATION);
-				}
-				if (inputText.charAt(i) != '"') {
-					position = i + 1;
-					return new Token(Token.TokenType.IDENTIFIER, "f");
-				}
+				if (result == null) {throw new UndefinedSymbolException();}
+				return result;
+				//if (inputText.charAt(i) != '"') {
+				//	position = i + 1;
+				//	return new Token(Token.TokenType.IDENTIFIER, "f");
+				//}
 			}
 
 		}
