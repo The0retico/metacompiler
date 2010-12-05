53a8771df08fa789ecd4161e4d2aba62902cc981
gscerbak <gscerbak@451a3750-404f-e429-ab1e-3de3acf2b62b>
2010-12-05 07:16:03 +0000

    Improved implementation of skipping blanks (whitespaces and newlines), created new token types, reimplemented scanning of keywords, implemented scanning of numbers and identifiers, added tests, minor refactoring, added comments.

--- GeneratorOfCompilers/src/lexer/ebnf/Lexer.java
+++ GeneratorOfCompilers/src/lexer/ebnf/Lexer.java
@@ -1,7 +1,5 @@
 package lexer.ebnf;
 
-import lexer.ebnf.Keyword.UndefinedSymbolException;
-
 /**
  * Lexer/scanner for the metagrammar language (EBNF).
  * 
@@ -9,70 +7,198 @@ import lexer.ebnf.Keyword.UndefinedSymbolException;
  * 
  */
 public class Lexer {
-	public enum LexerState {
-		READSTRING, READNUMBER, READIDENTIFIER, READSYMBOL
-	}
-
 	/**
 	 * Text to be scanned for tokens.
 	 */
-	private final String inputText;
-
-	private final LexerState state = LexerState.READSYMBOL;
+	private final String input;
 
 	/**
-	 * Current position of the scanner in the inputText.
+	 * Current position of the scanner in the current line of input.
+	 */
+	private int currentRow;
+
+	/**
+	 * Current index position in the input.
 	 */
 	private int position;
 
 	/**
-	 * Constant for white-space characters.
+	 * Current scanned token from input.
 	 */
-	private static final char WHITE_SPACE = ' ';
+	private IToken currentToken;
 
 	/**
-	 * @param input
+	 * Counter for current line.
+	 */
+	private int currentLine;
+
+	/**
+	 * @param inputText
 	 *            text to be scanned
 	 */
-	public Lexer(final String input) {
-		inputText = input;
+	public Lexer(final String inputText) {
+		input = inputText;
+		currentLine = 1;
+		currentRow = 1;
 		position = 0;
 	}
 
 	/**
-	 * @return the next symbol in the InputText or throw an exception otherwise
-	 * @throws StringIndexOutOfBoundsException
-	 *             if there are no more symbols in the inputText
-	 */
-	private char findNextSymbol() throws StringIndexOutOfBoundsException {
-		while (position < inputText.length()
-				&& inputText.charAt(position) == WHITE_SPACE) {
-			position++;
-		}
-		return inputText.charAt(position);
-	}
-
-	/**
 	 * @return next token in the inputText
-	 * @throws UndefinedSymbolException
+	 * @throws Exception
 	 *             if there is not a valid token
 	 */
-	public final Keyword getNextToken() throws UndefinedSymbolException {
-		final char nextSymbol = findNextSymbol();
-		position++;
-		return Keyword.create(nextSymbol);
+	public final IToken getNextToken() throws Exception {
+		skipBlankSymbols();
+		if (isKeywordNextToken()) {
+			currentToken = scanKeyword();
+		} else if (isNumberNextToken()) {
+			currentToken = scanNumber();
+		} else if (isIdentifierNextToken()) {
+			currentToken = scanIdentifier();
+		} else {
+			throw new Exception(currentLine + ":" + currentRow + ":'"
+					+ input.charAt(position)
+					+ "' Error! Does not start a token.");
+		}
+		return currentToken;
 	}
 
 	/**
 	 * @return true if the inputText contains another Token, false otherwise
 	 */
 	public final boolean hasNextToken() {
-		boolean result = true;
-		try {
-			findNextSymbol();
-		} catch (final StringIndexOutOfBoundsException e) {
-			result = false;
+		skipBlankSymbols();
+		return position < input.length();
+	}
+
+	/**
+	 * @param symbol
+	 *            to be checked if it is whitespace or newline
+	 * @return true if symbol is blank - whitespace or newline, false otherwise
+	 */
+	private boolean isBlank(final char symbol) {
+		return isWhiteSpace(symbol) || isNewLine(symbol);
+	}
+
+	/**
+	 * @return true if next token is an identifier, false otherwise.
+	 */
+	private boolean isIdentifierNextToken() {
+		final char nextSymbol = input.charAt(position);
+		return Character.isJavaIdentifierStart(nextSymbol);
+	}
+
+	/**
+	 * @return true if next token is a keyword, false otherwise
+	 */
+	private boolean isKeywordNextToken() {
+		final Keyword[] keywords = Keyword.values();
+		boolean found = false;
+		for (int index = 0; index < keywords.length && !found; index++) {
+			final Keyword keyword = keywords[index];
+			final int end = position - 1 + keyword.getLength();
+			if (end < input.length()) {
+				final String nextToken = input.substring(position, end + 1);
+				// System.out
+				// .println(keyword.getValue() + "\t'" + nextToken + "'");
+				found = nextToken.equals(keyword.getValue());
+			}
 		}
+		return found;
+	}
+
+	/**
+	 * @param symbol
+	 *            to be checked if it is a new line character
+	 * @return true if the symbol si a newline character, false otherwise
+	 */
+	private boolean isNewLine(final char symbol) {
+		return symbol == '\r' || symbol == '\n';
+	}
+
+	/**
+	 * @return true if next token is a number, false otherwise
+	 */
+	private boolean isNumberNextToken() {
+		final char nextSymbol = input.charAt(position);
+		return Character.isDigit(nextSymbol);
+	}
+
+	/**
+	 * @param symbol
+	 *            to be checked if it is a whitespace
+	 * @return true if symbol is whitespace, false otherwise
+	 */
+	private boolean isWhiteSpace(final char symbol) {
+		return symbol == ' ' || symbol == '\t';
+	}
+
+	/**
+	 * @return next scanned identifier token
+	 */
+	private IToken scanIdentifier() {
+		int end = position + 1;
+		while (end < input.length()
+				&& Character.isJavaIdentifierPart(input.charAt(end))) {
+			end++;
+		}
+		final String name = input.substring(position, end);
+		final Identifier result = new Identifier(name);
+		currentRow += result.getLength();
+		position += result.getLength();
 		return result;
+	}
+
+	/**
+	 * @return a keyword as a next token
+	 */
+	private IToken scanKeyword() {
+		final Keyword[] keywords = Keyword.values();
+		Keyword result = null;
+		int index = 0;
+		for (; index < keywords.length && result == null; index++) {
+			final Keyword keyword = keywords[index];
+			final int end = position - 1 + keyword.getLength();
+			if (end < input.length()) {
+				final String nextToken = input.substring(position, end + 1);
+				if (nextToken.equals(keyword.getValue())) {
+					result = keyword;
+				}
+			}
+		}
+		currentRow += result.getLength();
+		position += result.getLength();
+		return result;
+	}
+
+	/**
+	 * @return number as the next token.
+	 */
+	private IToken scanNumber() {
+		int end = position + 1;
+		while (end < input.length() && Character.isDigit(input.charAt(end))) {
+			end++;
+		}
+		final String nextToken = input.substring(position, end);
+		final Number result = new Number(Integer.parseInt(nextToken));
+		currentRow += result.getLength();
+		position += result.getLength();
+		return result;
+	}
+
+	/**
+	 * Moves to the position in input until non-whitespace character is found.
+	 */
+	private void skipBlankSymbols() {
+		while (position < input.length() && isBlank(input.charAt(position))) {
+			if (isNewLine(input.charAt(position))) {
+				currentLine++;
+				currentRow = 1;
+			} else {
+				currentRow++;
+			}
+			position++;
+		}
 	}
 }
