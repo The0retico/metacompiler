f571c30955ce564a131dc5747eed43183b9fa9b7
gscerbak <gscerbak@451a3750-404f-e429-ab1e-3de3acf2b62b>
2011-02-13 23:28:05 +0000

    Created the Interpreter facade for use in MetaCompiler

--- trunk/LambdaInterpreter/src/sk/scerbak/lambdainterpreter/Interpreter.java
+++ trunk/LambdaInterpreter/src/sk/scerbak/lambdainterpreter/Interpreter.java
@@ -4,6 +4,6 @@ public class Interpreter {
 
 	public static final String execute(final String code) {
 		final IExpression expression = Parser.fromString(code);
-		return Printer.toString(expression);
+		return Printer.toString(expression.normalForm());
 	}
 }
